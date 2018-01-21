572cb77bb8daa45664a095204138c3e00357fe02
Cameron Sinko <csinko@gmail.com>
2018-01-20 22:22:45 -0500

    Working integration with Google Actions, can now play a show by an artist and the latest show by the artist

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ var sleep = require('sleep');
 var squeeze = new SqueezeServer('http://localhost', 9000);
 var request = require('request');
 var readline = require('readline');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = 8080;
+app.use(bodyParser.json());
+
 
 function playerIdByName(name, callback) {
   var found = false;
@@ -69,37 +76,86 @@ function setAlbum(url) {
 
 }
 
-function getShows(artist, callback) {
+function getShow(artist, callback) {
   url = "https://archive.org/advancedsearch.php"
   query = "collection:" + artist + " AND mediatype:etree";
-  filters = "fl[]=identifier&fl[]=venue&fl[]=date"
+  filters = "fl[]=identifier&fl[]=venue&fl[]=date&fl[]=year&fl[]=title&fl[]=creator"
+  sort = "sort[]=downloads desc"
 
-  completeURL = url + "?q=" + query + "&" + filters + "&rows=10&page=1&output=json";
+  completeURL = url + "?q=" + query + "&" + filters + "&" + sort + "&rows=10&page=1&output=json";
 
   request(completeURL, { json: true }, (err, res, body) => {
     if (err) {return console.log(err); }
-     callback(body.response.docs);
+    randomNum = Math.floor(Math.random() * Math.floor(10));
+     callback(body.response.docs[randomNum]);
   })
-
-
 }
+
+function getNewestShow(artist, callback) {
+  url = "https://archive.org/advancedsearch.php"
+  query = "collection:" + artist + " AND mediatype:etree";
+  filters = "fl[]=identifier&fl[]=venue&fl[]=date&fl[]=year&fl[]=title&fl[]=creator"
+  sort = "sort[]=date desc"
+
+  completeURL = url + "?q=" + query + "&" + filters + "&" + sort + "&rows=1&page=1&output=json";
+
+  request(completeURL, { json: true }, (err, res, body) => {
+    if (err) {return console.log(err); }
+     callback(body.response.docs[0]);
+  })
+}
+
+app.post('/', function(req, res) {
+  //Check which intent was triggered
+  if(req.body.result.metadata.intentName === "Play Music") {
+    //User is playing music, get artist and play
+    console.log("Intent: Play Music");
+    var artist = req.body.result.parameters.Artist;
+    //Remove the spaces so it conforms with how Archive indexes it as a collection
+    artist = artist.replace(/\s+/g, '');
+
+    //Check if should grab the latest show
+    var isNew = req.body.result.parameters.new;
+    if (isNew.length != 0) {
+      //Newest show requested
+      getNewestShow(artist, function(doc) {
+        //Log some info about the show
+        console.log(doc.creator + " - " + doc.date + " " + doc.venue);
+        setAlbum("https://archive.org/download/" + doc.identifier + "/" + doc.identifier + "_vbr.m3u");
+
+      });
+      res.send('RESPONSE');
+      return;
+    } 
+
+    //Grab a show
+    getShow(artist, function(doc) {
+      console.log(doc.creator + " - " + doc.date + " " + doc.venue);
+      setAlbum("https://archive.org/download/" + doc.identifier + "/" + doc.identifier + "_vbr.m3u");
+    });
+  }
+  res.send('RESPONSE');
+});
 
 
 squeeze.on('register', function() {
-  //  setAlbum("https://archive.org/download/vulfpeck2016-09-08.cmc621xt.sbd.matrix.flac24/vulfpeck2016-09-08.cmc621xt.sbd.matrix.flac24_vbr.m3u");
 
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+  //  const rl = readline.createInterface({
+  //    input: process.stdin,
+  //    output: process.stdout
+  //  });
+  //
+  //  rl.question("Type an Artist: ", (artist) => {
+  //    getShows(artist, function(docs) {
+  //      console.log(docs[0].identifier);
+  //      setAlbum("https://archive.org/download/" + docs[0].identifier + "/" + docs[0].identifier + "_vbr.m3u");
+  //      rl.close();
+  //    });
+  //  });
+  //
 
-  rl.question("Type an Artist: ", (artist) => {
-    getShows(artist, function(docs) {
-      console.log(docs[0].identifier);
-      setAlbum("https://archive.org/download/" + docs[0].identifier + "/" + docs[0].identifier + "_vbr.m3u");
-      rl.close();
-    });
-  });
 
+  app.listen(port);
+  console.log('API Started on port ' + port);
 });
 
